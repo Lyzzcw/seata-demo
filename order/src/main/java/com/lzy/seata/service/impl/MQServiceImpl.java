687791584fa9d55af7757c198672e59efdda84c5
liuzhuo <you@exa>
2023-03-10 11:12:09 +0800

    mq 事务 逻辑补充

--- order/src/main/java/com/lzy/seata/service/impl/MQServiceImpl.java
+++ order/src/main/java/com/lzy/seata/service/impl/MQServiceImpl.java
@@ -32,10 +32,11 @@ public class MQServiceImpl implements MQService {
     public void createOrder(final Order order, String transactionId) {
 
         //1.扣除库存
-        //1.1 数据库乐观锁模式:尝试扣减库存，扣减库存成功才会进行下单逻辑
-        //  update auction_auctions set
-        //  quantity = quantity-#count#
-        //  where auction_id = #itemId# and quantity >= #count#
+        //1.1 数据库乐观锁模式:尝试扣减库存，利用update的排他性确保事务性，避免超卖
+        // update store set store = store - #{deductStore } where (store-#{deductStore }) >= 0
+        // 行级锁的原因存在性能瓶颈，高并发会出现请求堵塞超时问题
+        // 优化方式：对于潜在的热点商品，可以将库存平均分为多行（比如M行），扣减库存时，随机在M行中选取一行库存数据进行扣减。该方案突破了数据库单行锁的瓶颈限制，解决了爆款商品的热点问题。
+
         //1.2 redis+lua脚本
         //1.3 redis watch命令 乐观锁
 
